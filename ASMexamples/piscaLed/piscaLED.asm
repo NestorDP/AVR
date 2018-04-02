2b036400763ea1f03cba9c4c1e2a40a47493eb7c
Nestor D. Pereira Neto <nestor-dp@hotmail.com>
2018-04-02 14:50:40 -0300

    Update piscaLED.asm

--- ASMexamples/piscaLed/piscaLED.asm
+++ ASMexamples/piscaLed/piscaLED.asm
@@ -8,16 +8,16 @@
 
  //PROGRAMA PARA PISCAR UM LED LIGADO AO PINO PB0 DO ATMEGA8 
 
-.include "m328pdef.inc"		//arquivo com as definições para o compilador 
+.include "m328pdef.inc"			//arquivo com as definições para o compilador 
 .equ LED = PB0				//LED é o substituto de PB0 na programação 
 
-.ORG 0x000					//endereço na memória flash de início de escrita do código
+.ORG 0x000				//endereço na memória flash de início de escrita do código
 
 INICIO:	LDI R17,3
 		LDI R16,0x04		//inicialização do SP na posição 0x045F da SRAM 
-		OUT SPH,R16			//registrador SPH = 0x04 
+		OUT SPH,R16		//registrador SPH = 0x04 
 		LDI R16,0x5F 
-		OUT SPL,R16			//registrador SPL = 0x5F 
+		OUT SPL,R16		//registrador SPL = 0x5F 
 		LDI R16,0xFF		//carrega R16 com o valor com 0xFF 
 		OUT DDRB,R16		//configura todos os pinos do PORTB como saída 
 
@@ -28,13 +28,13 @@ PRINCIPAL:
 		RCALL ATRASO		//chama a sub-rotina de atraso 
 		RJMP PRINCIPAL		//vai para PRINCIPAL 
 
-ATRASO:						//sub-rotina de atraso 
+ATRASO:					//sub-rotina de atraso 
 		LDI R19,0x02		//carrega R19 com o valor 0x02  
 volta:  
-		DEC R17				//decrementa R17, começa com 0x00 
-		BRNE volta			//enquanto R17 > 0 fica decrementando R17 
-		DEC R18				//decrementa R18, começa com 0x00 
-		BRNE volta			//enquanto R18 >0 volta de crementar R18 
-		DEC R19				//decrementa R19 
-		BRNE volta			//enquanto R19 >0 vai para volta 
-		RET					//retorna da sub-rotina 
+		DEC R17			//decrementa R17, começa com 0x00 
+		BRNE volta		//enquanto R17 > 0 fica decrementando R17 
+		DEC R18			//decrementa R18, começa com 0x00 
+		BRNE volta		//enquanto R18 >0 volta de crementar R18 
+		DEC R19			//decrementa R19 
+		BRNE volta		//enquanto R19 >0 vai para volta 
+		RET			//retorna da sub-rotina 
